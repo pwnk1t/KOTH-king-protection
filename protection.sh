093574fc70ec4f93c3f98b3cd78f0c1c7d8af740
pwnk1t <cyberdragon.kali@gmail.com>
2026-03-16 02:06:41 +0100

    Add files via upload

---
+++ protection.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+################################
+#       King Protection        #
+################################
+
+echo "[+] creating King protection⚙️"
+
+sleep 1
+
+read -p "Your Username: " USER
+
+echo "[+] installing..."
+
+
+cat <<EOF > /etc/systemd/system/KOTH.service
+[Unit]
+Description=System Update
+
+[Service]
+ExecStart=/bin/bash -c 'while true; do echo $USER > /root/king.txt; sleep 0.5; done'
+Restart=always
+RestartSec=1
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable KOTH.service
+systemctl start KOTH.service
+
+sleep 2
+
+echo "[+] protection created 👑"
+sleep 1
+echo "[+] if service is not running do -> systemctl start KOTH"
+sleep 1
+echo "[+] check systemctl status KOTH.service"
+sleep 1 
+echo " 🕷️ enjoy the game 🕷️
+sleep 1
+echo ""
